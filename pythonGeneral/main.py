f07cf5ba9caaa0746d32bb082f4a0665558c9651
DecimusStringus <dimslp@gmail.com>
2022-11-15 22:17:39 +0100

    Better code

--- pythonGeneral/main.py
+++ pythonGeneral/main.py
@@ -1,19 +1,22 @@
 # Taken from mission Acceptable Password I
 
-# def is_acceptable_password(password: str) -> bool:
-#     # your code here
-#     return (len(password)>6 and
-#     any([x.isdigit() for x in password]) and
-#     any([x.isalpha() for x in password]))
-p='t2esti1ng'
+def is_acceptable_password(password: str) -> bool:
+    # your code here
 
-print(sum(c.isdigit() for c in p))
+    if 6 < len(password) <= 9 and 0 < sum(c.isdigit() for c in password) < len(password) or 9 < len(password):
+        x = True
+    else:
+        x = False
+    return x
 
-# assert is_acceptable_password("short") == False
-# assert is_acceptable_password("muchlonger") == False
-# assert is_acceptable_password("ashort") == False
-# assert is_acceptable_password("muchlonger5") == True
-# assert is_acceptable_password("sh5") == False
-# assert is_acceptable_password("1234567") == False
-#
-# print("The mission is done! Click 'Check Solution' to earn rewards!")
+assert is_acceptable_password("short") == False
+assert is_acceptable_password("short54") == True
+assert is_acceptable_password("muchlonger") == True
+assert is_acceptable_password("ashort") == False
+assert is_acceptable_password("notshort") == False
+assert is_acceptable_password("muchlonger5") == True
+assert is_acceptable_password("sh5") == False
+assert is_acceptable_password("1234567") == False
+assert is_acceptable_password("12345678910") == True
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
