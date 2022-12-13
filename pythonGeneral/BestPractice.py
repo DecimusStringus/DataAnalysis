e9382dbda5f7194626ba8dfb669497cb89af8061
DecimusStringus <dimslp@gmail.com>
2022-12-13 20:48:33 +0100

    More tasks

--- pythonGeneral/BestPractice.py
+++ pythonGeneral/BestPractice.py
@@ -1,3 +1,6 @@
+"""
+Dictionary
+"""
 # Count items of a list using dictionary
 # items = [True, True, False, True, False]
 # my_dict = {item: items.count(item) for item in items}
@@ -6,6 +9,23 @@
 # max(my_dict, key=my_dict.get)
 # or
 # max(my_dict, key=lambda key: my_dict[key])
+
+"""
+List
+"""
+# create list based on another list
+# abs_values = [abs(item) for item in values]
+
+# create 2-dimensional array out of two lists
+# two_lists = list(zip(abs_values, values))
+
+# sort 2-dimensional array on subarray
+# sorted_list = sorted(two_lists, key=lambda x: x[1])
+
+# return subarray of multidimensional array
+# sub_list = [item[1] for item in two_lists]
+
+
 
 
 
