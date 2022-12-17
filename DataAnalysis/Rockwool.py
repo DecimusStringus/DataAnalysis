fb5ca14bed7aa1a86b9b20f8dc4847aa982c71ef
DecimusStringus <dimslp@gmail.com>
2022-12-17 11:33:21 +0100

    Lessons 1 and 2

--- DataAnalysis/Rockwool.py
+++ DataAnalysis/Rockwool.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 #import xlrd
 
 path = "C:/Users/48885/Documents/Python Knowledge/Rockwool/"
-rockwool = pandas.read_excel(path + 'Quotes-1817.xls', header=3, index_col=0).rename(columns={'Close': 'CloseA', 'Volume': 'VolumeA', 'Close.1': 'CloseB', 'Volume.1': 'VolumeB'})
+file_path = r"C:\Users\48885\Documents\Python Knowledge\Rockwool\Quotes-1817.xls"
+#rockwool = pandas.read_excel(path + 'Quotes-1817.xls', header=3, index_col=0).rename(columns={'Close': 'CloseA', 'Volume': 'VolumeA', 'Close.1': 'CloseB', 'Volume.1': 'VolumeB'})
+rockwool = pandas.read_excel(file_path, header=3, index_col=0).rename(columns={'Close': 'CloseA', 'Volume': 'VolumeA', 'Close.1': 'CloseB', 'Volume.1': 'VolumeB'})
 #rockwool = pandas.read_excel(path + 'Quotes-1817.xls', header=None, index_col=0, skiprows=4)
 rw_2022 = rockwool.loc['2022-01-01':'2022-12-31'].copy() # copy() helps to get rid of a warning message
 rw_2022['DiffA'] = rw_2022['CloseA'].shift(1) - rw_2022['CloseA']
@@ -13,7 +15,8 @@ rw_2022['ma50A'] = rw_2022['CloseA'].rolling(50).mean()
 rw_2022['ma20A'] = rw_2022['CloseA'].rolling(20).mean()
 rw_2022['ma50B'] = rw_2022['CloseB'].rolling(50).mean()
 rw_2022['ma20B'] = rw_2022['CloseB'].rolling(20).mean()
-
+rockwool.count()
+print(rockwool.count())
 plt.figure(figsize=(10, 8))
 
 rw_2022['CloseA'].plot(label='CloseA')
