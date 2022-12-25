5e1d719bfaf12797c700f313a732ab57c6266eca
DecimusStringus <dimslp@gmail.com>
2022-12-25 15:07:45 +0100

    Change structure

--- pythonGeneral/main.py
+++
@@ -1,7 +0,0 @@
-data = ["a", "b", "a", "c", "a", "b"]
-my_dict = {item: data.count(item) for item in data}
-result1 = max(my_dict, key=my_dict.get) # another way to do it
-result2 = max(my_dict, key=lambda var: my_dict[var])
-# result3 = sorted(data, key=)
-result4 = my_dict["a"]
-print(result2)
